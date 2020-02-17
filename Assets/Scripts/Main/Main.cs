8fce2a5738641a10638afd31f16a5ab57bb0bc64
shogo-makishima <48924544+shogo-makishima@users.noreply.github.com>
2020-02-17 17:59:27 +0300

    0.0.8
    -Небольшие фиксы;
    -Добавлен один уровень и один корабль;

--- Assets/Scripts/Main/Main.cs
+++ Assets/Scripts/Main/Main.cs
@@ -11,7 +11,7 @@ namespace Main {
     public class Main {
         [DllImport("__Internal")]
         private static extern void WindowAlert(string message);
-        public static bool _isDebug = true;
+        public static bool _isDebug = false;
         public static bool _wasLoad = false;
         public static string currentLevel = "Tutorial";
 
@@ -235,6 +235,7 @@ namespace Main {
             gameDetails.moneys = Player.moneys;
             gameDetails.levels = Levels.levels;
             gameDetails.currentLevel = Main.currentLevel;
+            gameDetails.currentShip = Player.currentShip;
 
             PlatformSafeMessage($"SaveFile: {JsonUtility.ToJson(gameDetails)} \nPath: {Application.persistentDataPath}/save.json");
 
